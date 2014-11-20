943a372a211b3271d3f46cb64b00f8e8a609249c
David DeSimone <djd231@scarletmail.rutgers.edu>
2014-11-20 16:42:55 -0500

    Added Crossing based organizational scheme

--- src/leg_scripts/graphex.py
+++ src/leg_scripts/graphex.py
@@ -2,7 +2,11 @@ import networkx as nx
 
 G = nx.Graph()
 
-G.add_edge(1,2)
-G.add_edge(1,2)
+G.add_edge(1,2, {'weight':1})
 
 print len(G.edges())
+print G[1][2]
+print G.has_edge(1,3)
+print G[1][2]['weight']
+G[1]['hello'] = False
+print G[1]['hello']
